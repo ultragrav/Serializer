9349f245f53cbdb7638804207827ac1e040731ec
BuildTools <petzmagnus@gmail.com>
2020-07-22 10:05:40 +0200

    Added gradle scripts

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2020. UltraDev
+ */
+
+group = "net.ultragrav"
+version = "1.0.0"
+
+plugins {
+    `java-library`
+    maven
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+}
+
+dependencies {
+    implementation("com.google.guava:guava-primitives:r03")
+
+    implementation("org.projectlombok:lombok:1.18.8")
+    annotationProcessor("org.projectlombok:lombok:1.18.8")
+}

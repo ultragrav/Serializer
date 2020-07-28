b71ee8bc1787b93d90a263d547e402ae18e325c6
mattlack15 <private@mattlack.ca>
2020-07-28 16:31:06 -0400

    af

--- src/main/java/net/ultragrav/serializer/GravSerializable.java
+++ src/main/java/net/ultragrav/serializer/GravSerializable.java
@@ -1,14 +1,27 @@
 package net.ultragrav.serializer;
 
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Map;
 
 public interface GravSerializable {
-    static Object deserializeObject(GravSerializer serializer, Object... otherArguments) {
+
+    Map<String, String> relocationMappings = new HashMap<>();
+
+    static Object deserializeObject(net.ultragrav.serializer.GravSerializer serializer, Object... otherArguments) {
+
         String className = serializer.readString();
+        for(Map.Entry<String, String> mapping : relocationMappings.entrySet()) {
+            className = className.replace(mapping.getKey(), mapping.getValue());
+        }
+
         Class<?>[] argumentTypes = new Class<?>[otherArguments.length + 1];
         Object[] arguments = new Object[otherArguments.length + 1];
-        argumentTypes[0] = GravSerializer.class;
+        argumentTypes[0] = net.ultragrav.serializer.GravSerializer.class;
         arguments[0] = serializer;
         for (int i = 0; i < otherArguments.length; i++) {
             argumentTypes[i + 1] = otherArguments[i].getClass();
@@ -29,10 +42,10 @@ public interface GravSerializable {
         }
     }
 
-    static void serializeObject(GravSerializer serializer, GravSerializable serializable) {
+    static void serializeObject(net.ultragrav.serializer.GravSerializer serializer, GravSerializable serializable) {
         serializer.writeString(serializable.getClass().getName());
         serializable.serialize(serializer);
     }
 
-    void serialize(GravSerializer serializer);
+    void serialize(net.ultragrav.serializer.GravSerializer serializer);
 }
